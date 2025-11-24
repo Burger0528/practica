5a5394e1713614d285a0bc5cc707bbd7a95656a8
Burger0528 <andreshurtado3000@icloud.com>
2025-11-24 09:00:21 -0500

    Clear initial inventory data
    Removed initial inventory items from the data structure.

--- simulacro1/data.py
+++ simulacro1/data.py
@@ -3,49 +3,7 @@
 
 inventario = [
     {
-        "id": 1,
-        "nombre": "Televisor LED 50\"",
-        "marca": "Samsung",
-        "categoria": "Televisores",
-        "precio": 1800000,
-        "stock": 10,
-        "garantia": 12
-    },
-    {
-        "id": 2,
-        "nombre": "Portátil Gamer",
-        "marca": "Asus",
-        "categoria": "Computadores",
-        "precio": 4500000,
-        "stock": 5,
-        "garantia": 24
-    },
-    {
-        "id": 3,
-        "nombre": "Audífonos Bluetooth",
-        "marca": "Sony",
-        "categoria": "Audio",
-        "precio": 300000,
-        "stock": 20,
-        "garantia": 6
-    },
-    {
-        "id": 4,
-        "nombre": "Mouse Inalámbrico",
-        "marca": "Logitech",
-        "categoria": "Accesorios",
-        "precio": 60000,
-        "stock": 30,
-        "garantia": 12
-    },
-    {
-        "id": 5,
-        "nombre": "Teclado Mecánico",
-        "marca": "Redragon",
-        "categoria": "Accesorios",
-        "precio": 200000,
-        "stock": 15,
-        "garantia": 18
+      
     }
 ]
 
